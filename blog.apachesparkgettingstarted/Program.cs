f76ee4a45614fecaecd54c6511f33fb77b812a39
Samuele Resca <samuele.resca@just-eat.com>
2019-09-08 14:38:28 +0100

    first commit

---
+++ blog.apachesparkgettingstarted/Program.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.Spark.Sql;
+
+
+namespace blog.apachesparkgettingstarted
+{
+   class Program
+    {
+        static void Main(string[] args)
+        {
+             SparkSession spark = SparkSession
+                            .Builder()
+                            .AppName("My application")
+                            .GetOrCreate();
+             
+            DataFrame df = spark
+                .Read()
+                .Option("header", true)
+                .Csv("./data/seattle-cultural-space-inventory.csv");
+        }
+    }
+}
+//spark-submit --class org.apache.spark.deploy.dotnet.DotnetRunner  --master local microsoft-spark-2.4.x-0.4.0.jar    dotnet blog.apachesparkgettingstarted.dll
