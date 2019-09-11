d209a64047063f68091b5d7771576406eef1d7c3
Samuele Resca <samuele.resca@gmail.com>
2019-09-11 08:32:03 +0100

    Updating the code

--- blog.apachesparkgettingstarted/Program.cs
+++ blog.apachesparkgettingstarted/Program.cs
@@ -3,19 +3,24 @@
 
 namespace blog.apachesparkgettingstarted
 {
-   class Program
+    class Program
     {
         static void Main(string[] args)
         {
-             SparkSession spark = SparkSession
-                            .Builder()
-                            .AppName("My application")
-                            .GetOrCreate();
-             
+            SparkSession spark = SparkSession
+                           .Builder()
+                           .AppName("My application")
+                           .GetOrCreate();
+
             DataFrame df = spark
                 .Read()
                 .Option("header", true)
                 .Csv("./data/seattle-cultural-space-inventory.csv");
+
+            df
+                .Select("Name", "Phone")
+                .Filter(df["Phone"].IsNotNull())
+                .Show();
         }
     }
 }
